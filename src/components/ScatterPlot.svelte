b415efcca3ab013d4170a7842542941999e5acb9
bhakin <103210246+bhakin@users.noreply.github.com>
2024-06-07 20:22:10 -0700

    update

--- src/components/ScatterPlot.svelte
+++ src/components/ScatterPlot.svelte
@@ -133,16 +133,14 @@
     const xScale = d3.scaleLinear().domain([0,6]).range([0, width])
     const yScale = d3.scaleLinear().domain([0,6]).range([height,0])
 
-    const x1 = d3.min(filteredData, d => d.release_spin_rate) - 100;
-    const x2 = d3.max(filteredData, d => d.release_spin_rate) + 100;
-    const y1 = (-w[0] * x1 - b) / w[1];
-    const y2 = (-w[0] * x2 - b) / w[1];
+    const x1 = xScale.domain();
+    const x2 = x1.map(x => (-w[0] * x - b) / w[1]);
 
     svg.append('line')
-      .attr('x1', x(x1))
-      .attr('y1', y(y1))
-      .attr('x2', x(x2))
-      .attr('y2', y(y2))
+      .attr('x1', xScale(x1[0] + 2.75))
+      .attr('y1', yScale(x2[0] + 2.75))
+      .attr('x2', xScale(x1[1] + 2.75))
+      .attr('y2', yScale(x2[1] + 2.75))
       .attr('stroke', 'green')
       .attr('stroke-width', 2);
     
@@ -176,6 +174,7 @@
 <div id="chart-container">
   <div id="scatterplot"></div>
   <div id="tooltip" style="display: none; position: absolute; background: white; border: 1px solid #000; padding: 5px; border-radius: 5px; font-size: 14px; color: black;"></div>
+
 </div>
 
 <style>
